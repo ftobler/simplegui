b705c34862e3ba7b0227a9e78d24df5144fa4124
Florin Tobler <florin.tobler@hotmail.com>
2024-06-21 21:02:42 +0200

    try out more tests

---
+++ tests/test_gui.py
@@ -0,0 +1,43 @@
+import os
+
+# set a vitrual framebuffer for headless actions runners.
+if os.name != "nt" and os.getenv("GITHUB_ACTIONS"):
+    os.system('Xvfb :1 -screen 0 1600x1200x16  &')
+    os.environ["DISPLAY"] = ":1.0"
+
+# after that import simplegui and tkinter with it
+import simplegui as sg
+
+import sys
+sys.path.insert(0, "./examples/DemoPrograms")
+sys.path.insert(0, "../examples/DemoPrograms")
+
+import time
+import pytest
+
+
+
+
+# patch the window read function to actually exit pytest controlled
+class ExitException(Exception):
+    pass
+
+def exit_function(*_args, **_kwargs):
+    time.sleep(1)
+    raise ExitException()
+
+sg.Window.read = exit_function
+
+
+
+
+
+
+
+
+def test_demo_all_elements():
+    import Demo_All_Elements
+    with pytest.raises(ExitException):
+        Demo_All_Elements.main()
+
+
